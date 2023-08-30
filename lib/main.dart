357ffbef5d7274b418a7544f9b3320e90b0f250a
Fateme Arjomandi <farjomandy76@gmail.com>
2023-08-30 16:05:32 +0330

    add font & image & pakage flutter_localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -11,41 +12,53 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa'), //persion
+        //Locale('en'), // Spanish
+      ],
+      theme: ThemeData(fontFamily: "Rubik"),
       home: Scaffold(
         appBar: AppBar(
-          actions: const [
-            SizedBox(
-              width: 8,
-            ),
-            Expanded(
-                child: Align(
-                    alignment: Alignment.centerLeft, child: Icon(Icons.menu))),
-            Align(
+          backgroundColor: Colors.white,
+          actions: [
+            Image.asset("assets/image/icon.png"),
+            const Align(
                 alignment: Alignment.centerRight,
                 child: Text(
                   'قیمت به روز ارز',
                   style: TextStyle(color: Colors.black),
                 )),
-            Icon(Icons.macro_off_rounded)
+            Expanded(
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Image.asset("assets/image/menu.png"))),
+            const SizedBox(
+              width: 8,
+            ),
           ],
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text(
+                Image.asset('assets/image/g.png'),
+                const SizedBox(
+                  width: 8,
+                ),
+                const Text(
                   'نرخ ارز آزاد چیست؟',
                   style: TextStyle(),
                 ),
-                SizedBox(
-                  width: 8,
-                ),
-                Icon(Icons.macro_off)
               ],
             ),
-            Text(
+            const Text(
               ' نرخ ارزها در معادلات نقدی و رایج روزانه است معادلات  نقدی معادلاتی هستند که خریدار و فروشنده به محض انجام معادله ارز و ریال رو باهم مبادله می کنند',
               textDirection: TextDirection.rtl,
               style: TextStyle(color: Colors.black),
