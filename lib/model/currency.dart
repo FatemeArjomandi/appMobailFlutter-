49b3f192815766efad92b9fc9f2755572ab919c9
Fateme Arjomandi <farjomandy76@gmail.com>
2023-09-10 14:21:37 +0430

    add field id

--- lib/model/currency.dart
+++ lib/model/currency.dart
@@ -1,10 +1,12 @@
 class Currency {
+  String? id;
   String? title;
   String? price;
   String? chenge;
   String? status;
   Currency(
-      {required this.title,
+      {required this.id,
+      required this.title,
       required this.price,
       required this.chenge,
       required this.status});
