3109c49f30b7232b3db173763d1f0e8996574e2b
Fateme Arjomandi <farjomandy76@gmail.com>
2023-09-25 12:48:58 +0330

    add image & build class travelAppScreen

---
+++ lib/screen/travel_app_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TravelApp extends StatefulWidget {
+  const TravelApp({super.key});
+
+  @override
+  State<TravelApp> createState() => _TravelAppState();
+}
+
+class _TravelAppState extends State<TravelApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        body: Column(),
+      );
+      
+  }
+}
