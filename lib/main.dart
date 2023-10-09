8497e1a16bd66b06b40d95185586b71020a965f0
Fateme Arjomandi <farjomandy76@gmail.com>
2023-10-09 13:21:28 +0330

    build/splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+Future main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
+  
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
