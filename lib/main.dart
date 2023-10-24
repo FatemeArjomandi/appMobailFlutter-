49c0c68cada7c9d01f485727043ef8e769765768
Fateme Arjomandi <farjomandy76@gmail.com>
2023-10-24 19:28:48 +0330

    remove package splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-
-Future main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  FlutterNativeSplash.remove();
+void main() {
   runApp(const MyApp());
-  
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          actions: const [
-            SizedBox(
-              width: 8,
-            ),
-            Expanded(
-                child: Align(
-                    alignment: Alignment.centerLeft, child: Icon(Icons.menu))),
-            Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  'قیمت به روز ارز',
-                  style: TextStyle(color: Colors.black),
-                )),
-            Icon(Icons.macro_off_rounded)
-          ],
-        ),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'نرخ ارز آزاد چیست؟',
-                  style: TextStyle(),
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                Icon(Icons.macro_off)
-              ],
-            ),
-            Text(
-              ' نرخ ارزها در معادلات نقدی و رایج روزانه است معادلات  نقدی معادلاتی هستند که خریدار و فروشنده به محض انجام معادله ارز و ریال رو باهم مبادله می کنند',
-              textDirection: TextDirection.rtl,
-              style: TextStyle(color: Colors.black),
-            )
-          ]),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
   }
 }
