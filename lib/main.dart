ee266a08d25f652a6500140fe7ada7079867b2e4
Fateme Arjomandi <farjomandy76@gmail.com>
2023-10-26 13:42:23 +0330

    using fluttergen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:tech_blog/gen/assets.gen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +11,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
-        localizationsDelegates: [
+        localizationsDelegates: const [
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
           GlobalCupertinoLocalizations.delegate,
         ],
-        supportedLocales: [
+        supportedLocales: const [
           Locale('fa'), //persion
           //Locale('en'), // Spanish
         ],
-        home: Scaffold());
+        theme: ThemeData(
+            fontFamily: "Rubik",
+            textTheme: const TextTheme(
+              displayLarge: TextStyle(
+                  fontFamily: 'Rubik',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w900),
+              bodyLarge: TextStyle(
+                  fontFamily: 'Rubik',
+                  fontSize: 13,
+                  fontWeight: FontWeight.w300),
+              displayMedium: TextStyle(
+                  fontFamily: 'Rubik',
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w300),
+              displaySmall: TextStyle(
+                  fontFamily: 'Rubik',
+                  color: Colors.red,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700),
+              headlineMedium: TextStyle(
+                  fontFamily: 'Rubik',
+                  color: Colors.green,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700),
+            )),
+        home: Scaffold(
+          body: Center(child:Image(image: Assets.image.logo.provider())
+          ,),
+        ));
   }
 }
